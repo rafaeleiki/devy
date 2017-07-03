abf1c2d088f805c03a30bedf31cd69759ab1569e
Rafael <rafaeleiki@yahoo.com.br>
2017-07-03 17:07:15 -0300

    Adds projects menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {PROJECTS} from './project/projects.data';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'DevCotuca';
+  projects = PROJECTS;
 }
