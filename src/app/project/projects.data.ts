db552070999c06f93116258808fe08a6258094e2
Rafael <rafaeleiki@yahoo.com.br>
2017-07-03 17:49:07 -0300

    Removes video files from repo

--- src/app/project/projects.data.ts
+++ src/app/project/projects.data.ts
@@ -6,7 +6,7 @@ export const PROJECTS = [
     description: `Porque não juntar as funcionalidades
       de agenda e plano de estudos em
       um único aplicativo?`,
-    video: 'alpha-study.mp4',
+    video: 'https://drive.google.com/uc?export=download&id=0BxgB0meBhPaqZjlnUGI1Wk5EME0',
     image: 'alpha-study.png',
   },
   {
@@ -15,14 +15,14 @@ export const PROJECTS = [
     team: 'Jonaphael, Pedro, Yohann - PD16 (vespertino)',
     description: `Dispositivo para leitura de braile
       em uma única célula.`,
-    video: 'braile-reader.mp4',
+    video: 'https://drive.google.com/uc?export=download&id=0BxgB0meBhPaqRWpFSTRRZnFBSDg',
   },
   {
     id: 3,
     title: 'Interface Visual de Proteínas',
     team: 'Gabriel, Guilherme, Júlia - PD16 (matutino)',
     description: `Visualização de proteínas em interface visual.`,
-    video: 'proteinas_compress.mp4',
+    video: 'https://drive.google.com/uc?export=download&id=0BxgB0meBhPaqbVZ6aWZ5ZkEzak0',
   },
   {
     id: 4,
@@ -30,7 +30,7 @@ export const PROJECTS = [
     team: 'Emílio e Henrique - PD15 (matutino)',
     description: `Aprendizado de programação com
       uso de mascote em realidade aumentada.`,
-    video: 'simas.mp4',
+    video: 'https://drive.google.com/uc?export=download&id=0BxgB0meBhPaqb0NqcF82RUIxNUk',
   },
   {
     id: 5,
@@ -38,7 +38,7 @@ export const PROJECTS = [
     team: 'Igor e Vitor - PD16 (matutino)',
     description: `Inclusão digital de seniores por meio
       de análise de aspectos de interface.`,
-    video: 'sine_compress.mp4',
+    video: 'https://drive.google.com/uc?export=download&id=0BxgB0meBhPaqeFJBNXlVV1ZCYlk',
   },
   {
     id: 6,
@@ -47,6 +47,6 @@ export const PROJECTS = [
     description: `Criação de uma plataforma para interligação de
       ONGs, voluntários e doadores, colaborando com a criação, articulação e
       desenvolvimento destas iniciativas sociais.`,
-    video: 'solidari.mp4',
+    video: 'https://drive.google.com/uc?export=download&id=0BxgB0meBhPaqbjIxd3NkVk1QVms',
   },
 ];
