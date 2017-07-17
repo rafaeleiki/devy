0e045094d39fc97873d5a76d5961ed37846a64f8
Rafael <rafaeleiki@yahoo.com.br>
2017-07-17 18:46:57 -0300

    Adds achievements badges for projects

--- src/app/project/projects.data.ts
+++ src/app/project/projects.data.ts
@@ -1,4 +1,31 @@
-export const PROJECTS = [
+import {Achievement, Project} from '../project';
+
+const researchPlan: Achievement = {
+  icon: 'search',
+  description: 'Concluiu o plano de pesquisa',
+};
+
+const program: Achievement = {
+  icon: 'directions_run',
+  description: 'Participou de um programa externo para desenvolver o projeto',
+};
+
+const support: Achievement = {
+  icon: 'supervisor_account',
+  description: 'Possui algum apoio externo para o projeto',
+};
+
+const interfacePrototypes: Achievement = {
+  icon: 'style',
+  description: 'Concluiu os protótipos das interfaces',
+};
+
+const development: Achievement = {
+  icon: 'code',
+  description: 'Está em fase de desenvolvimento'
+};
+
+export const PROJECTS: Project[] = [
   {
     id: 1,
     title: 'Alpha Study',
@@ -8,6 +35,9 @@ export const PROJECTS = [
       um único aplicativo?`,
     video: 'https://drive.google.com/uc?export=download&id=0BxgB0meBhPaqZjlnUGI1Wk5EME0',
     image: 'alpha-study.png',
+    achievements: [
+      development,
+    ],
   },
   {
     id: 2,
@@ -16,6 +46,7 @@ export const PROJECTS = [
     description: `Dispositivo para leitura de braile
       em uma única célula.`,
     video: 'https://drive.google.com/uc?export=download&id=0BxgB0meBhPaqRWpFSTRRZnFBSDg',
+    achievements: [],
   },
   {
     id: 3,
@@ -23,6 +54,7 @@ export const PROJECTS = [
     team: 'Gabriel, Guilherme, Júlia - PD16 (matutino)',
     description: `Visualização de proteínas em interface visual.`,
     video: 'https://drive.google.com/uc?export=download&id=0BxgB0meBhPaqbVZ6aWZ5ZkEzak0',
+    achievements: [],
   },
   {
     id: 4,
@@ -31,6 +63,10 @@ export const PROJECTS = [
     description: `Aprendizado de programação com
       uso de mascote em realidade aumentada.`,
     video: 'https://drive.google.com/uc?export=download&id=0BxgB0meBhPaqb0NqcF82RUIxNUk',
+    achievements: [
+      development,
+      support,
+    ],
   },
   {
     id: 5,
@@ -39,6 +75,10 @@ export const PROJECTS = [
     description: `Inclusão digital de seniores por meio
       de análise de aspectos de interface.`,
     video: 'https://drive.google.com/uc?export=download&id=0BxgB0meBhPaqeFJBNXlVV1ZCYlk',
+    achievements: [
+      program,
+      researchPlan,
+    ],
   },
   {
     id: 6,
@@ -48,5 +88,10 @@ export const PROJECTS = [
       ONGs, voluntários e doadores, colaborando com a criação, articulação e
       desenvolvimento destas iniciativas sociais.`,
     video: 'https://drive.google.com/uc?export=download&id=0BxgB0meBhPaqbjIxd3NkVk1QVms',
+    achievements: [
+      program,
+      interfacePrototypes,
+      development,
+    ],
   },
 ];
