594bd64e36c58a0749861c7b75ca899764f32605
GuiBrandt <gui.g.brandt@gmail.com>
2017-10-14 19:42:12 -0300

    Mudanças feitas conforme sugerido.

--- src/app/project/2017/isopin.ts
+++ src/app/project/2017/isopin.ts
@@ -1,21 +1,25 @@
 import {Project} from '../../project';
-import {development, researchPlan, support} from '../achievements';
+import {development, researchPlan, support, interfacePrototypes, nationalFairFinalist } from '../achievements';
 
 export const isopin: Project = {
   id: 3,
   title: 'ISOPIN',
   team: 'Gabriel, Guilherme, Júlia - PD16 (matutino)',
-  image: 'isopin.png';
+  image: 'isopin.png',
   advisor: 'Patrícia Gagliardo de Campos',
   mentors: ['Marcelo Mendes Brandão'],
-  shortDescription: `Sistema de busca e visualização gráfica de interações proteicas entre espécies vegetais.`,
-  description: `Sistema de busca e visualização gráfica de interações proteicas entre espécies vegetais que procura diminuir o custo de produção de alimentos transgênicos uma vez que apresenta uma maior gama de proteínas a serem manipuladas, facilitando o trabalho dos pesquisadores da área de biologia molecular.`,
+  shortDescription: `Sistema de busca e visualização gráfica de interações 
+	proteicas entre espécies vegetais.`,
+  description: `Sistema de busca e visualização gráfica de interações 
+	proteicas entre espécies vegetais. Procura, através de uma ferramenta que
+	facilita o trabalho dos pesquisadores da área de biologia molecular, 
+	diminuir o custo de produção de alimentos transgênicos.`,
   video: 'https://www.youtube.com/embed/mdhpdorGUTQ',
   achievements: [
     researchPlan,
     support,
     development,
     interfacePrototypes,
-    nationalFairFinalist(['Mostractc 2017']),
+    nationalFairFinalist(['Mostractc 2017'])
   ],
 };
