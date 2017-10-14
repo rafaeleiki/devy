3d6385aa02e9a6f8e83ec9a3f880e12af744213b
Igormandello <igormandello@gmail.com>
2017-10-14 16:13:31 -0300

    Updates the Sine project page
    - Adds image asset
    - Changes description
    - Changes achievements

--- src/app/project/2017/sine.ts
+++ src/app/project/2017/sine.ts
@@ -3,7 +3,8 @@ import {
   development,
   nationalFairFinalist,
   program,
-  researchPlan
+  researchPlan,
+  interfacePrototypes
 } from '../achievements';
 
 export const sine: Project = {
@@ -14,13 +15,14 @@ export const sine: Project = {
   mentors: [],
   shortDescription: `Inclusão digital de seniores por meio
       de análise de aspectos de interface.`,
-  description: `Inclusão digital de seniores por meio
-      de análise de aspectos de interface.`,
+  description: `Sabendo do grande crescimento da população idosa no Brasil e no mundo, vimos o problema de que grande parte dos idosos não conseguem utilizar ou instalar apps sem a ajuda de familiares, como netos e filhos. Para resolver esse problema, que tira a independência do grupo, teremos um aplicativo, que identificará o app sendo usado no momento e fará tutoriais de como atingir cada funcionalidade, tudo isso de forma objetiva e intuitiva, tudo para facilitar a compreensão.`,
   video: 'https://www.youtube.com/embed/2_mPCaNDSi4',
+  image: 'sine.png',
   achievements: [
     program,
     researchPlan,
     development,
+    interfacePrototypes,
     nationalFairFinalist(['Febrat 2017', 'Mostractc 2017']),
   ],
 };
