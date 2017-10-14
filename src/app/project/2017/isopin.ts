e47c79e2d7e51e7d1423e7572d98d8c656fb43ce
GuiBrandt <gui.g.brandt@gmail.com>
2017-10-14 17:35:55 -0300

    Página do ISOPIN arrumada

--- src/app/project/2017/isopin.ts
+++ src/app/project/2017/isopin.ts
@@ -3,16 +3,19 @@ import {development, researchPlan, support} from '../achievements';
 
 export const isopin: Project = {
   id: 3,
-  title: 'Interface Visual de Proteínas',
+  title: 'ISOPIN',
   team: 'Gabriel, Guilherme, Júlia - PD16 (matutino)',
+  image: 'isopin.png';
   advisor: 'Patrícia Gagliardo de Campos',
   mentors: ['Marcelo Mendes Brandão'],
-  shortDescription: `Visualização de proteínas em interface visual.`,
-  description: `Visualização de proteínas em interface visual.`,
+  shortDescription: `Sistema de busca e visualização gráfica de interações proteicas entre espécies vegetais.`,
+  description: `Sistema de busca e visualização gráfica de interações proteicas entre espécies vegetais que procura diminuir o custo de produção de alimentos transgênicos uma vez que apresenta uma maior gama de proteínas a serem manipuladas, facilitando o trabalho dos pesquisadores da área de biologia molecular.`,
   video: 'https://www.youtube.com/embed/mdhpdorGUTQ',
   achievements: [
     researchPlan,
     support,
     development,
+    interfacePrototypes,
+    nationalFairFinalist(['Mostractc 2017']),
   ],
 };
