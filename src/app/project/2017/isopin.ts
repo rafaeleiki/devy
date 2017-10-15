03843f85d6b2c3d576c7e9392971e36e9f328021
Guilherme Brandt <GuiBrandt@users.noreply.github.com>
2017-10-15 16:51:19 -0200

    Update isopin.ts
    Trailing whitespace removido

--- src/app/project/2017/isopin.ts
+++ src/app/project/2017/isopin.ts
@@ -8,12 +8,12 @@ export const isopin: Project = {
   image: 'isopin.png',
   advisor: 'Patrícia Gagliardo de Campos',
   mentors: ['Marcelo Mendes Brandão'],
-  shortDescription: `Sistema de busca e visualização gráfica de interações 
-	proteicas entre espécies vegetais.`,
-  description: `Sistema de busca e visualização gráfica de interações 
-	proteicas entre espécies vegetais. Procura, através de uma ferramenta que
-	facilita o trabalho dos pesquisadores da área de biologia molecular, 
-	diminuir o custo de produção de alimentos transgênicos.`,
+  shortDescription: `Sistema de busca e visualização gráfica de interações
+	 proteicas entre espécies vegetais.`,
+  description: `Sistema de busca e visualização gráfica de interações
+	 proteicas entre espécies vegetais. Procura, através de uma ferramenta que
+	facilita o trabalho dos pesquisadores da área de biologia molecular,
+	 diminuir o custo de produção de alimentos transgênicos.`,
   video: 'https://www.youtube.com/embed/mdhpdorGUTQ',
   achievements: [
     researchPlan,
