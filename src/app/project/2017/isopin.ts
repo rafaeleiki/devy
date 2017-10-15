be965db197b4f098d793f31cd7d137d2e59f2903
Guilherme Brandt <GuiBrandt@users.noreply.github.com>
2017-10-15 19:21:40 -0200

    Página do ISOPIN arrumada (#11)
    * Página do ISOPIN arrumada
    
    * Mudanças feitas conforme sugerido.
    
    * Update isopin.ts
    
    Trailing whitespace removido

--- src/app/project/2017/isopin.ts
+++ src/app/project/2017/isopin.ts
@@ -1,18 +1,25 @@
 import {Project} from '../../project';
-import {development, researchPlan, support} from '../achievements';
+import {development, researchPlan, support, interfacePrototypes, nationalFairFinalist } from '../achievements';
 
 export const isopin: Project = {
   id: 3,
-  title: 'Interface Visual de Proteínas',
+  title: 'ISOPIN',
   team: 'Gabriel, Guilherme, Júlia - PD16 (matutino)',
+  image: 'isopin.png',
   advisor: 'Patrícia Gagliardo de Campos',
   mentors: ['Marcelo Mendes Brandão'],
-  shortDescription: `Visualização de proteínas em interface visual.`,
-  description: `Visualização de proteínas em interface visual.`,
+  shortDescription: `Sistema de busca e visualização gráfica de interações
+	 proteicas entre espécies vegetais.`,
+  description: `Sistema de busca e visualização gráfica de interações
+	 proteicas entre espécies vegetais. Procura, através de uma ferramenta que
+	facilita o trabalho dos pesquisadores da área de biologia molecular,
+	 diminuir o custo de produção de alimentos transgênicos.`,
   video: 'https://www.youtube.com/embed/mdhpdorGUTQ',
   achievements: [
     researchPlan,
     support,
     development,
+    interfacePrototypes,
+    nationalFairFinalist(['Mostractc 2017'])
   ],
 };
