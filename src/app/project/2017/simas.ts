ca8032b0eccf3d627dbedc66d7543b47f53918ac
Henrique Mineto <henrique.mineto@gmail.com>
2017-10-15 19:24:13 -0200

    Alterações na página do projeto Simas! (#12)
    * Alterações na página do projeto Simas!
    
    * Correção da string multilinha e tamanho da imagem

--- src/app/project/2017/simas.ts
+++ src/app/project/2017/simas.ts
@@ -10,13 +10,18 @@ import {
 export const simas: Project = {
   id: 4,
   title: 'Simas!',
+  image: 'simas.png',
   team: 'Emílio e Henrique - PD15 (matutino)',
   advisor: 'Simone Pierini Facini Rocha',
   mentors: ['Sérgio Luiz Moral Marques'],
   shortDescription: `Aprendizado de programação com
       uso de mascote em realidade aumentada.`,
-  description: `Aprendizado de programação com
-      uso de mascote em realidade aumentada.`,
+  description: `Aplicativo para o aprendizado de
+		lógica de programação voltado para crianças
+		com uso de mascote em realidade aumentada que,
+		através de uma linguagem de blocos, busca
+		introduzir conceitos da área de informática
+		durante o desenvolvimento dos usuários.`,
   video: 'https://www.youtube.com/embed/f0Yp_ojQt1o',
   achievements: [
     development,
