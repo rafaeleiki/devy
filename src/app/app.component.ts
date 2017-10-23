d0c071d5a6779d6462deb599f6fdc3b789107b53
Rafael <rafaeleiki@yahoo.com.br>
2017-10-23 00:11:04 -0200

    Fixes Analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 import {PROJECTS} from './project/projects.data';
+import {NavigationEnd, Router} from '@angular/router';
+
+function _window(): any {
+  return window;
+}
 
 @Component({
   selector: 'app-root',
@@ -9,4 +14,14 @@ import {PROJECTS} from './project/projects.data';
 export class AppComponent {
   title = 'DevCotuca';
   projects = PROJECTS;
+
+  constructor(public router: Router) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        const window = _window();
+        window.ga('set', 'page', event.urlAfterRedirects);
+        window.ga('send', 'pageview');
+      }
+    });
+  }
 }
